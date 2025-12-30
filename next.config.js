9ea2d7c42597f06cceadd21304438bad58ead083
shammah <shamaj2006@gmail.com>
2025-12-30 19:38:05 +0100

    Final Commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true
-  }
-}
+  output: 'export',          // <-- this tells Next to export as static HTML
+  images: { unoptimized: true },  // <-- disable Next.js image optimization for static export
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
